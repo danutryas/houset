fca1927407866f1319523dacfa63f20ccb5ed59e
Danutryas <Danupristowo09@gmail.com>
2022-06-06 18:33:29 +0700

    Moving function to Input context

--- src/components/Modal/DeleteProduct.jsx
+++ src/components/Modal/DeleteProduct.jsx
@@ -1,19 +1,15 @@
+import axios from 'axios'
 import React, { useContext } from 'react'
 import InputContext from '../Context/getInput'
 import ModalContext from '../Context/ModalContext'
 
 function DeleteProduct() {
     const {closeModal,deleteModal} = useContext(ModalContext)
-    const {datas,setDatas,selectedData} = useContext(InputContext)
-
-    const deleteProductData = () => {
-        setDatas(datas.filter((el) => el.id !== selectedData.id))
-        closeModal()
-    }
+    const {deleteProductData} = useContext(InputContext)
+    
 
   return (
     <aside className={`delete modal ${deleteModal ? "active":""}`}>
-        {console.log(deleteModal)}
         <h1>Delete Product</h1>
         <p>Are you sure you want to delete this product?</p>
         <div className="cta">
