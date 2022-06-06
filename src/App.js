9bdcb169298dd89043992a45b36a566134d8d5b1
Danutryas <Danupristowo09@gmail.com>
2022-06-06 14:28:41 +0700

    Inserting context to app and editing modal container styles

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-} from "react-router-dom";import './App.css';
+} from "react-router-dom";
+import './App.css';
 import Header from './components/Header';
 import Catalog from "./pages/Catalog";
 import Homepage from './pages/Homepage';
+import CombinedContext from "./components/Context/CombinedContext";
+import './styles/css/modal.css'
+
+import { useContext } from "react";
+import ModalContext from "./components/Context/ModalContext";
+import ModalComponent from "./components/ModalComponent";
 
 function App() {
+
   return (
-    <div className="App">
-      <Router>
-        <Header />
-        <Routes >
-          <Route path="/" element={<Homepage />} />
-          <Route path="/catalog" element={<Catalog />} />
-        </Routes>
-      </Router>
-    </div>
+    <CombinedContext>
+      <div className="App">
+        <Router>
+          <Header />
+          <Routes >
+            <Route path="/" element={<Homepage />} />
+            <Route path="/catalog" element={<Catalog />} />
+          </Routes>
+        </Router>
+      </div>
+      {/* hidden Modal */}
+      <ModalComponent />
+    </CombinedContext>
   );
 }
 
