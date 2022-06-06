63161b094aca483d492390fb22fcd0047e26ea0f
Danutryas <Danupristowo09@gmail.com>
2022-06-06 18:34:05 +0700

    Remove temporary  datas and implementing CRUD

--- src/components/Context/getInput.jsx
+++ src/components/Context/getInput.jsx
@@ -1,102 +1,55 @@
+import  axios  from "axios";
 import { createContext, useContext, useEffect, useState} from "react";
 import ModalContext from "./ModalContext";
 
 const InputContext = createContext({});
 
 export function InputProvider({children}) {
-    const [datas,setDatas] = useState([
-    {
-        "id": "p001",
-        "name": "Meja Kantor",
-        "type": "Meja",
-        "price": "500000",
-        "qty": "10"
-    },
-    {
-        "id": "p002",
-        "name": "Meja Kamar",
-        "type": "Meja",
-        "price": "250000",
-        "qty": "20"
-    },
-    {
-        "id": "p003",
-        "name": "Sofa Ruang Keluarga",
-        "type": "Sofa",
-        "price": "600000",
-        "qty": "4"
-    },
-    {
-        "id": "p004",
-        "name": "Kursi Kantor",
-        "type": "Kursi",
-        "price": "100000",
-        "qty": "40"
-    },
-    {
-        "id": "p005",
-        "name": "Sofa Tamu",
-        "type": "Sofa",
-        "price": "700000",
-        "qty": "2"
-    },
-    {
-        "id": "p006",
-        "name": "Kasur Double",
-        "type": "Kasur",
-        "price": "220000",
-        "qty": "7"
-    },
-    {
-        "id": "p007",
-        "name": "Kasur Single",
-        "type": "Kasur",
-        "price": "150000",
-        "qty": "14"
-    },
-    {
-        "id": "p008",
-        "name": "Meja Makan",
-        "type": "Meja",
-        "price": "1000000",
-        "qty": "10"
-    },
-    {
-        "id": "p009",
-        "name": "Lemari Baju",
-        "type": "Lemari",
-        "price": "400000",
-        "qty": "10"
-    },
-    {
-        "id": "p010",
-        "name": "Lemari Pajangan",
-        "type": "Lemari",
-        "price": "500000",
-        "qty": "20"
-    }
-])
+    const [datas,setDatas] = useState([])
     const [selectedData,setSelectedData] = useState({})
     const [name,setName] = useState("")
     const [type,setType] = useState("")
     const [price,setPrice] = useState(0)
     const [quantity,setQuantity] = useState(0)
-
-    const {setModal,setModifyModal,setDeleteModal,setAddModal,closeModal} = useContext(ModalContext);
+    
+    const {closeModal} = useContext(ModalContext);
+    
+    const url = "http://localhost:3004"
 
 
     const addProduct = () => {
         const nextId = getNextId()
+
+        axios.post(`${url}/product`,{
+            id: `p${nextId}`,
+            name,type,price,
+            qty:quantity
+        })
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+
         setDatas(() => [...datas,{
             id: `p${nextId}`,
             name,type,price,
             qty:quantity
         }])     
+
         clearInput()
         closeModal()
     }
+
+    
     const changeProduct = () => {
         const currData = selectedData
+        const id = currData.id
+        axios.put(`${url}/product/${id}`,
+            {
+                name,type,price,
+                qty:quantity
+            })
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err))
+
         setDatas(
             datas.map((data)=> {
                 if(currData.id === data.id) {
@@ -112,6 +65,17 @@ export function InputProvider({children}) {
         closeModal()
         clearInput()
     }
+    const deleteProductData = () => {
+        const id = selectedData.id
+
+        axios.delete(`${url}/product/${id}`)
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+        setDatas(datas.filter((el) => el.id !== id))
+
+        closeModal()
+    }
+
     const clearInput = () => {
         setSelectedData({})
         setName("")
@@ -121,30 +85,27 @@ export function InputProvider({children}) {
     }
 
     const getNextId = () => {
-
         const lastItem = datas.slice(-1) 
         const getId = lastItem[0].id.slice(1)
         let intId = parseInt(getId)
         intId++
         if (intId < 10) {
-            return "00"+intId.toString()
+            return "00" + intId.toString()
         }else if (intId >= 10 && intId < 100) {
-            return "0"+intId.toString()   
+            return "0" + intId.toString()   
         }else {
             return intId.toString()
         }
     } 
-
-
     useEffect(() => {
-        console.log(selectedData)
-    },[selectedData])
-    useEffect(() => {
-      console.log(datas)
-    },[datas])
+        axios.get(`${url}/product`) 
+        .then((data)=> setDatas(data.data))
+        .catch((error)=> console.log(error))
+    },[])
+
 
     return(
-        <InputContext.Provider value={{datas,setDatas,name,setName,type,setType,price,setPrice,quantity,setQuantity,addProduct,selectedData,setSelectedData,changeProduct}}>
+        <InputContext.Provider value={{deleteProductData,datas,setDatas,name,setName,type,setType,price,setPrice,quantity,setQuantity,addProduct,selectedData,setSelectedData,changeProduct}}>
             {children}
         </InputContext.Provider>
     )
